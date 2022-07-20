f8578731b9812fabf808ce14b9465d24f13c7454
KateMMiller <kathryn_miller@nps.gov>
2022-07-20 15:08:56 -0400

    new external scripts for reports

---
+++ generate_reports_from_script_MIDN.R
@@ -0,0 +1,17 @@
+
+path = c("D:/NETN/Monitoring_Projects/Forest_Health/2022_data/Maps_and_Data/Weekly_Checks/")
+
+
+# Knit weekly reports
+rmarkdown::render('Weekly_QC_check_MIDN.Rmd',
+                  output_file = paste0(path, "Weekly_QC_check_MIDN_Week1.html"),
+                  params = list(week_start = '2022-07-17',
+                                cycle_latest_num = 5,
+                                locType = 'all')) 
+
+# Knit QAQC Report
+rmarkdown::render('QAQC_report_MIDN.Rmd', 
+                  output_file = paste0(path, "QAQC_report_MIDN", "_VAFO-999-2022.html"),
+                  params = list(year = 2022, plot = 'VAFO-999', locType = 'all'))
+
+
---
+++ generate_reports_from_script_NETN.R
@@ -0,0 +1,41 @@
+
+path = c("D:/NETN/Monitoring_Projects/Forest_Health/2022_data/Maps_and_Data/Weekly_Checks/")
+
+
+# Knit weekly reports
+rmarkdown::render('Weekly_QC_check_NETN.Rmd',
+                  output_file = paste0(path, "Weekly_QC_check_NETN_Week8_ACAD.html"),
+                  params = list(week_start = '2022-07-17',
+                                cycle_latest_num = 5,
+                                locType = 'all')) 
+
+# Knit QAQC Report
+rmarkdown::render('QAQC_report_NETN.Rmd', 
+                  output_file = paste0(path, "QAQC_report_NETN", "_ACAD-172-2022.html"),
+                  params = list(year = 2022, plot = 'ACAD-172', locType = 'all'))
+
+
+# Knit weekly reports
+rmarkdown::render('Weekly_QC_check_NETN.Rmd',
+                  output_file = paste0(path, "Weekly_QC_check_NETN_Week8_ACAD.html"),
+                  params = list(week_start = '2022-07-17',
+                               cycle_latest_num = 5,
+                               locType = 'all')) 
+
+# Knit QAQC Report
+rmarkdown::render('QAQC_report_NETN.Rmd', 
+                  output_file = paste0(path, "QAQC_report_NETN", "_ACAD-172-2022.html"),
+                  params = list(year = 2022, plot = 'ACAD-172', locType = 'all'))
+
+rmarkdown::render('QAQC_report_NETN.Rmd', 
+                  output_file = paste0(path, "QAQC_report_NETN", "_ACAD-014-2022.html"),
+                  params = list(year = 2022, plot = 'ACAD-014', locType = 'all'))
+
+rmarkdown::render('QAQC_report_NETN.Rmd', 
+                  output_file = paste0(path, "QAQC_report_NETN", "_ACAD-015-2022.html"),
+                  params = list(year = 2022, plot = 'ACAD-015', locType = 'all'))
+
+rmarkdown::render('QAQC_report_NETN.Rmd', 
+                  output_file = paste0(path, "QAQC_report_NETN", "_MIMA-013-2022.html"),
+                  params = list(year = 2022, plot = 'MIMA-013', locType = 'all'))
+
