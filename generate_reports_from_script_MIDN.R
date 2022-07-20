f9c53dabfa2e1d5814dc809a5aefcc8fbe7f899e
KateMMiller <kathryn_miller@nps.gov>
2022-07-20 15:24:20 -0400

    changed current to cycle 4

--- generate_reports_from_script_MIDN.R
+++ generate_reports_from_script_MIDN.R
@@ -6,7 +6,7 @@ path = c("D:/NETN/Monitoring_Projects/Forest_Health/2022_data/Maps_and_Data/Week
 rmarkdown::render('Weekly_QC_check_MIDN.Rmd',
                   output_file = paste0(path, "Weekly_QC_check_MIDN_Week1.html"),
                   params = list(week_start = '2022-07-17',
-                                cycle_latest_num = 5,
+                                cycle_latest_num = 4,
                                 locType = 'all')) 
 
 # Knit QAQC Report
