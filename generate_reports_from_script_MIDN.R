bb0e15bd6b20bb729012865d115925e6d3498e55
KateMMiller <kathryn_miller@nps.gov>
2022-09-27 09:15:08 -0400

    added code for ASIS

--- generate_reports_from_script_MIDN.R
+++ generate_reports_from_script_MIDN.R
@@ -3,15 +3,22 @@ path = c("D:/NETN/Monitoring_Projects/Forest_Health/2022_data/Maps_and_Data/Week
 
 
 # Knit weekly reports
+# For ASIS visit 1
 rmarkdown::render('Weekly_QC_check_MIDN.Rmd',
-                  output_file = paste0(path, "Weekly_QC_check_MIDN_Week1_T3.html"),
-                  params = list(week_start = '2022-07-24',
+                  output_file = paste0(path, "Weekly_QC_check_MIDN_Week9.html"),
+                  params = list(week_start = '2022-09-18',
+                                cycle_latest_num = 1,
+                                locType = 'VS')) 
+# For MIDN
+rmarkdown::render('Weekly_QC_check_MIDN.Rmd',
+                  output_file = paste0(path, "Weekly_QC_check_MIDN_Week8.html"),
+                  params = list(week_start = '2022-09-11',
                                 cycle_latest_num = 4,
-                                locType = 'all')) 
+                                locType = 'VS')) 
 
 # Knit QAQC Report
 rmarkdown::render('QAQC_report_MIDN.Rmd', 
-                  output_file = paste0(path, "QAQC_report_MIDN", "_VAFO-9999-2022.html"),
-                  params = list(year = 2022, plot = 'VAFO-9999', locType = 'all'))
+                  output_file = paste0(path, "QAQC_report_MIDN", "_FRSP-281-2022.html"),
+                  params = list(year = 2022, plot = 'FRSP-281', locType = 'all'))
 
 
