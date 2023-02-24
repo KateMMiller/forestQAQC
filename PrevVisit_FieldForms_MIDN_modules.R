61f1f5134e2fb2a1ffbbf42ff6e5edf5a2241fbd
Kate Miller <kathryn_miller@nps.gov>
2023-02-24 16:02:16 -0500

    updated for MIDN

---
+++ PrevVisit_FieldForms_MIDN_modules.R
@@ -0,0 +1,186 @@
+#------------------------------------------------
+# Generate Previous Visit Field Forms by Module
+#------------------------------------------------
+
+# The scripts below will save paged html files of each plot per park and year
+# to a FieldForms folder on your desktop. If The Field_Forms folder doesn't
+# exist on your desktop, it will be added. Same goes for NETN_Tree_Maps folder
+
+path <- paste0(file.path(Sys.getenv("USERPROFILE"),"Desktop",fsep="\\"), 
+               "\\Field_Forms\\MIDN\\")
+
+# Park-level tree and quad data go into "Field_Forms"
+if(!dir.exists(path)){dir.create(path)}
+
+# Individual tree and quad plot files go into "Field_Forms/indiv" folder.
+if(!dir.exists(paste0(path, "indiv"))){
+  dir.create(paste0(path, "\\indiv\\"))}
+
+# Individual full visit of plot files go into "Field_Forms/indiv_all" folder.
+if(!dir.exists(paste0(path, "indiv_all"))){
+  dir.create(paste0(path, "indiv_all\\"))}
+
+# Tree maps go into "NETN_Tree_Maps" folder on Desktop
+path_trmaps <- paste0(file.path(Sys.getenv("USERPROFILE"),"Desktop",fsep="\\"), 
+                      "\\MIDN_Tree_Maps\\")
+
+if(!dir.exists(path_trmaps)){dir.create(path_trmaps)}
+
+#----- Imports/libraries -----
+library(tidyverse)
+library(knitr)
+library(rmarkdown)
+library(forestMIDN)
+library(pagedown)
+library(pdftools)
+
+source("PrevVisit_functions.R")
+importData()
+
+#----- Tree Maps -----
+park_4 <- c("APCO", "BOWA", "COLO", "GETT", "HOFU", "VAFO") # Panel 4, sampled 2018
+park_1 <- c("FRSP", "GEWA", "PETE", "RICH", "THST") # Panel 1, sampled 2019
+SAHI <- "SAHI" # Sampled 2017
+
+plotTreeMap(park = park_4, from = 2018, to = 2018, 
+            path = path_trmaps, output_to = 'file')
+
+plotTreeMap(park = park_1, from = 2019, to = 2019, 
+            path = path_trmaps, output_to = 'file')
+
+plotTreeMap(park = "SAHI", from = 2017, to = 2017,
+            path = path_trmaps, output_to = 'file')
+
+#params <- data.frame(parkcode = "APCO", plot_name = "APCO-262", yearpv = 2018)
+#params <- data.frame(parkcode = "FRSP", plot_name = "FRSP-002", yearpv = 2019)
+
+#----- Park-level Tree and Quadrat reports -----
+##----- Render Functions to iterate on -----
+render_reports <- function(plot, pv_year, rmd, name){
+  park = substr(plot, 1, 4)
+  plotname = plot
+  render(input = rmd,
+         params = list(plot_name = plotname, 
+                       year = as.numeric(pv_year), 
+                       print = TRUE),
+         output_file = paste0(path, "indiv\\", 
+                              plot, "_", pv_year, "_", name, ".html"))
+  }
+
+rmdtr <- "PrevVisit_3A_Tree_Measurements.Rmd"
+rmdqd <- "PrevVisit_4A_Quadrat_Data_MIDN.Rmd"
+
+# Since revisits are different years, you have to run for each set, then reset params 
+# and run for next set.
+##----- params for panel 4 -----
+park <- c("APCO", "BOWA", "COLO", "GETT", "HOFU", "VAFO") # Panel 4, sampled 2018
+year = 2018
+
+##----- Source from compile script -----
+source("PrevVisit_FieldForms_MIDN_compile.R") 
+plots <- sort(unique(plotevs$Plot_Name)) # plot list to iterate on below
+
+##----- Render Reports -----
+map(plots, ~render_reports(., pv_year = year, rmdtr, "Trees")) # trees
+map(plots, ~ render_reports(., pv_year = year, rmdqd, "Quads")) # quads
+
+##----- params for panel 1 -----
+park <- c("FRSP", "GEWA", "PETE", "RICH", "THST") # Panel 1, sampled 2019
+year = 2019
+
+##----- Source from compile script -----
+source("PrevVisit_FieldForms_MIDN_compile.R") 
+plots <- sort(unique(plotevs$Plot_Name)) # plot list to iterate on below
+
+##----- Render Reports -----
+map(plots, ~render_reports(., pv_year = year, rmdtr, "Trees")) # trees
+map(plots, ~ render_reports(., pv_year = year, rmdqd, "Quads")) # quads
+
+##----- params for SAHI -----
+SAHI <- "SAHI" # Sampled 2017
+year = 2017
+
+##----- Source from compile script -----
+source("PrevVisit_FieldForms_MIDN_compile.R") 
+plots <- sort(unique(plotevs$Plot_Name)) # plot list to iterate on below
+
+##----- Render Reports -----
+map(plots, ~render_reports(., pv_year = year, rmdtr, "Trees")) # trees
+map(plots, ~ render_reports(., pv_year = year, rmdqd, "Quads")) # quads
+
+#----- Sapling reports by parks -----
+##----- Generates park-level reports, so don't have a bunch of blank space b/t page breaks. 
+park_4 <- c("APCO", "BOWA", "COLO", "GETT", "HOFU", "VAFO") # Panel 4, sampled 2018
+park_1 <- c("FRSP", "GEWA", "PETE", "RICH", "THST") # Panel 1, sampled 2019
+SAHI <- "SAHI" # Sampled 2017
+
+parks <- c(park_4, park_1, SAHI)
+years = c(rep(2018, length(park_4)), rep(2019, length(park_1)), 2017)
+
+#++++++++ STILL WORKING ON THIS +++++++++++
+# collapse_rows doesn't handle page breaks well, so instead I want to 
+# generate plot-level reports that are all in the same html with a loop
+# I just need to work out knit_child code to do that
+#
+# rmdsap <- "PrevVisit_2A_Microplot_Sapling_Data_MIDN_long.Rmd"
+# 
+# render_saps <- function(parkcode, pv_year){
+#   render(input = rmdsap,
+#          params = list(park = parkcode,
+#                        year = as.numeric(pv_year), 
+#                        print = TRUE),
+#          output_file = paste0(path, park, "_", pv_year, "_Sapling", ".html"))
+# }
+# 
+# map2(parks, years, ~render_saps(.x, .y))
+
+#----- Convert Tree, Sapling, & Quad htmls to pdf -----
+##----- Convert individual html to pdf (ignore warnings unless it doesn't work) -----
+html_list <- list.files(paste0(path, "indiv\\"), pattern = '.html', full.names = T)
+pdf_list <- paste0(substr(html_list, 1, nchar(html_list) - 4), "pdf")
+walk2(html_list, pdf_list, ~pagedown::chrome_print(.x, .y))
+
+##----- Combine park-level pdfs into 1 pdf per module -----
+nhps <- c("MABI", "MIMA", "SAGA", "SARA")
+
+combine_tree_pdfs <- function(park, year, path){
+  pdf_list <- list.files(paste0(path, "\\indiv\\"), pattern = ".pdf", full.names = TRUE)
+  trees <- pdf_list[grep(("Trees"), pdf_list)]
+  park_trees <- trees[grep((park), trees)]
+  pdftools::pdf_combine(input = park_trees,
+                        output = paste0(path, park, "_", year, "_Trees.pdf"))
+}
+
+purrr::map2(parks, years, ~combine_tree_pdfs(.x, .y, path))
+
+
+combine_quad_pdfs <- function(park, year, path){
+  pdf_list <- list.files(paste0(path, "\\indiv\\"), pattern = ".pdf", full.names = TRUE)
+  trees <- pdf_list[grep(("Quads"), pdf_list)]
+  park_trees <- trees[grep((park), trees)]
+  pdftools::pdf_combine(input = park_trees,
+                        output = paste0(path, park, "_", year, "_Quads.pdf"))
+}
+
+purrr::map2(parks, years, ~combine_quad_pdfs(.x, .y, path))
+
+#---- Render plot viewers for each park -----
+source("PrevVisit_FieldForms_NETN_compile.R") 
+parks = c("ACAD", "MABI", "MIMA", "SAGA", "SARA")
+years = c(2019, rep(2018, 4))
+
+render_viewer <- function(park, year){
+  park_code = park
+  yr = as.numeric(year)
+  render(input = "PrevVisit_Plot_Viewer_NETN_All.Rmd",
+         params = list(parkcode = park_code, 
+                       yearpv = yr, 
+                       print = FALSE),
+         output_file = paste0(path, park, "_", yr, "_Plot_Viewer.html"))
+}
+
+#render_poss <- possibly(.f = render_viewer, otherwise = NULL)
+
+# running through a few at a time b/c bogs down laptop
+purrr::map2(parks[c(1:3,5)], years[c(1:3,5)], ~render_viewer(.x, .y))
+
