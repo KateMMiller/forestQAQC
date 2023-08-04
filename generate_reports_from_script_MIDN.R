42af7369b095d2656e427bed8ac1dc808d1a9c56
Kate Miller <kathryn_miller@nps.gov>
2023-08-04 08:37:14 -0400

    updated for 2023

--- generate_reports_from_script_MIDN.R
+++ generate_reports_from_script_MIDN.R
@@ -5,13 +5,13 @@ path = c("C:/NETN/Monitoring_Projects/Forest_Health/2023_data/Maps&Data/")
 # Knit weekly reports
 # For ASIS visit 1
 rmarkdown::render('Weekly_QC_check_MIDN.Rmd',
-                  output_file = paste0(path, "Weekly_QC_check_MIDN_Week9.html"),
-                  params = list(week_start = '2022-09-18',
+                  output_file = paste0(path, "Weekly_QC_check_MIDN_ASIS.html"),
+                  params = list(week_start = '2023-09-10',
                                 cycle_latest_num = 1,
                                 locType = 'VS')) 
 # For MIDN
 rmarkdown::render('Weekly_QC_check_MIDN.Rmd',
-                  output_file = paste0(path, "Weekly_QC_check_MIDN_Week1.html"),
+                  output_file = paste0(path, "Weekly_QC_check_MIDN_VAFO_HOFU_GETT.html"),
                   params = list(week_start = '2023-07-16',
                                 cycle_latest_num = 4,
                                 locType = 'VS')) 
